36d54657a7f4eee186bed33d09b3cae54dad4cdc
WenyuZhu <wzz0015@auburn.edu>
2022-01-21 13:08:09 -0600

    create getall and getbyid endpoints for categories and vendors

---
+++ src/test/java/guru/springframework/spring5webfluxrest/controllers/CategoryControllerTest.java
@@ -0,0 +1,49 @@
+package guru.springframework.spring5webfluxrest.controllers;
+
+import guru.springframework.spring5webfluxrest.domains.Category;
+import guru.springframework.spring5webfluxrest.repositories.CategoryRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static org.junit.Assert.*;
+
+public class CategoryControllerTest {
+
+    WebTestClient webTestClient;
+    CategoryController categoryController;
+    CategoryRepository categoryRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        categoryRepository = Mockito.mock(CategoryRepository.class);
+        categoryController = new CategoryController(categoryRepository);
+        webTestClient = WebTestClient.bindToController(categoryController).build();
+    }
+
+    @Test
+    public void getAllCategories() {
+        BDDMockito.given(categoryRepository.findAll())
+                .willReturn(Flux.just(Category.builder().description("Cat1").build(),
+                        Category.builder().description("Cat2").build()));
+        webTestClient.get()
+                .uri("/api/v1/categories/")
+                .exchange()
+                .expectBodyList(Category.class)
+                .hasSize(2);
+    }
+
+    @Test
+    public void getCategoryById() {
+        BDDMockito.given(categoryRepository.findById("someid"))
+                .willReturn(Mono.just(Category.builder().description("Cat1").build()));
+        webTestClient.get()
+                .uri("/api/v1/categories/someid/")
+                .exchange()
+                .expectBody(Category.class);
+    }
+}
